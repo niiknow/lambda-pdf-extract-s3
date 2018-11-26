aab13ce1b8dc51ec7aefe8c9faaadf9ddbec81a0
noogen <tom@noogen.net>
2018-11-26 02:30:53 -0600

    migrate some codes

---
+++ bin/index.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cwd=$(pwd)
+file_dpi=$1
+work_dir=$2
+width=$3
+filename=$4
+
+cd "$work_dir"
+$cwd/bin/pdftoppm -jpeg -r $file_dpi -scale-to-x 1000 -scale-to-y -1 index.pdf jpeg-1000-page
+$cwd/bin/pdftoppm -jpeg -r $file_dpi -scale-to-x $width -scale-to-y -1 index.pdf "jpeg-$width-page"
+$cwd/bin/pdftohtml -p -xml -hidden index.pdf
+$cwd/bin/pdftoppm -r $file_dpi index.pdf ppm-page
+cp -f jpeg-1000-page-1.jpg index.jpg || true
+cp -f jpeg-1000-page-01.jpg index.jpg || true
+cp -f jpeg-1000-page-1.jpg "$filename" || true
