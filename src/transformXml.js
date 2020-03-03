742ed6358fc4a2b02b6ab35caaabe4060df8fcff
noogen <tom@noogen.net>
2020-03-03 11:19:02 -0600

    support right side only crop

--- src/transformXml.js
+++ src/transformXml.js
@@ -44,7 +44,7 @@ export default async (event) => {
     page.width  = scaleW
     page.height = Math.floor(page.scale * page.oldsize.height)
 
-    if (!mcBox.scale && (mcBox.CropBox.x > 0 || mcBox.CropBox.y > 0)) {
+    if (!mcBox.scale && (mcBox.CropBox.x > 0 || mcBox.CropBox.y > 0 || mcBox.CropBox.xx < mcBox.MediaBox.xx)) {
       if ((mcBox.CropBox.width !== mcBox.MediaBox.width) || (mcBox.CropBox.height !== mcBox.MediaBox.height)) {
 
         // this should be around 1.5 pixels to 1 pt/point but we calculate it anyway
